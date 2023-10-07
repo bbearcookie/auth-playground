c425cf203b459c5688d707ecf353ea5157ac163f
bbearcookie <aodem@naver.com>
2023-10-08 00:28:52 +0900

    feat: 테스트용 user 추가 API에 유효성 검사 zod 적용

--- backend/src/handlers/users/post.ts
+++ backend/src/handlers/users/post.ts
@@ -1,11 +1,25 @@
 import { Handler } from 'express';
 import User from '@/models/User';
+import { z } from 'zod';
+
+const requestSchema = z.object({
+  body: z.object({
+    name: z.string(),
+    email: z.string().email(),
+    avatar: z.string().url(),
+  }),
+});
+
+type RequestData = z.infer<typeof requestSchema>;
 
 const handler: Handler = async (req, res) => {
+  const { body } = req as unknown as RequestData;
+  const { name, email, avatar } = body;
+
   const user = new User({
-    name: 'bear',
-    email: 'bill@initech.com',
-    avatar: 'https://i.imgur.com/dM7Thhn.png',
+    name,
+    email,
+    avatar,
   });
 
   await user.save();
