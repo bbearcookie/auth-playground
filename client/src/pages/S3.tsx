432d118ac5fd44513dd4d74df7f05b273dce446f
bbearcookie <aodem@naver.com>
2024-03-01 12:01:07 +0900

    feat: s3, cloudfront 각각 버튼 추가

--- client/src/pages/S3.tsx
+++ client/src/pages/S3.tsx
@@ -1,15 +1,17 @@
 import axios from 'axios';
-import React from 'react';
 import { Link } from 'react-router-dom';
 
-const IMAGE_URL =
+const S3_IMAGE_URL =
   'https://bearcookiestudy-image.s3.ap-northeast-2.amazonaws.com/big-bear.jpg';
 
+const CLOUDFRONT_IMAGE_URL =
+  'https://d3odjmr288vgvd.cloudfront.net/big-bear.jpg';
+
 const S3 = () => {
-  const handleClick = async () => {
+  const handleDownload = async (targetUrl: string) => {
     console.log('TODO: 이미지 다운로드 로직 구현하기..');
 
-    const response = await axios.get(IMAGE_URL, {
+    const response = await axios.get(targetUrl, {
       responseType: 'blob',
     });
 
@@ -29,13 +31,22 @@ const S3 = () => {
   return (
     <div>
       <h1>S3 버킷에 업로드 된 이미지를 다운로드합니다.</h1>
-      <img src={IMAGE_URL} alt="큰 곰 이미지" />
+      <img src={CLOUDFRONT_IMAGE_URL} alt="큰 곰 이미지" />
       <div>
         <Link to="/">메인으로 가기</Link>
-        <a href={IMAGE_URL} download>
+        <a href={CLOUDFRONT_IMAGE_URL} download>
           a태그로 다운로드 고고
         </a>
-        <button onClick={handleClick}>이미지 다운로드 해보기</button>
+        <div>
+          <button onClick={() => handleDownload(S3_IMAGE_URL)}>
+            S3 이미지 다운로드 해보기
+          </button>
+        </div>
+        <div>
+          <button onClick={() => handleDownload(CLOUDFRONT_IMAGE_URL)}>
+            Cloudfront 이미지 다운로드 해보기
+          </button>
+        </div>
       </div>
     </div>
   );
