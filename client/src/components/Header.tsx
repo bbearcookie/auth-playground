1a4e87bfb3a3d6e3bd3329dbae0dd7809b261b6b
bbearcookie <aodem@naver.com>
2024-03-11 02:48:24 +0900

    chore: GA에서 수집하고자 auth-check-button className 추가

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,13 +2,20 @@ import { useContext } from 'react';
 import { AuthContext } from '@/contexts/AuthContext';
 
 const Header = () => {
-  const { isLoggedIn, user, handleCheck, handleLogout } = useContext(AuthContext);
+  const { isLoggedIn, user, handleCheck, handleLogout } =
+    useContext(AuthContext);
 
   return (
     <header>
       <h1>못생긴 헤더</h1>
-      <div>{isLoggedIn ? `어서오세요 ${user.username}님` : `당신은 로그인을 안하셨군요!`}</div>
-      <button onClick={handleCheck}>로그인 확인</button>
+      <div>
+        {isLoggedIn
+          ? `어서오세요 ${user.username}님`
+          : `당신은 로그인을 안하셨군요!`}
+      </div>
+      <button className="auth-check-button" onClick={handleCheck}>
+        로그인 확인
+      </button>
       {isLoggedIn && <button onClick={handleLogout}>로그아웃</button>}
     </header>
   );
